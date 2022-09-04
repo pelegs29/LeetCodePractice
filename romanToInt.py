7d24f0836051f06e7ac5a7f43c797aa4caf76be8
peleg <pelegs29@gmail.com>
2022-09-04 13:45:15 +0300

    Initial commit

---
+++ romanToInt.py
@@ -0,0 +1,11 @@
+def romanToInt(s):
+    dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    sum = 0
+    s = s.replace("IV", "IIII").replace("IX", "VIIII")
+    s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+    s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+    for x in s:
+        sum += dict[x]
+    print(sum)
+
+romanToInt("MCDLXXVI")
