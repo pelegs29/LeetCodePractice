70ae9cd13f5dffdd854b95f10c26402569659f57
peleg <pelegs29@gmail.com>
2022-11-08 11:20:44 +0200

    c practice

---
+++ Easy/isPalindrome.c
@@ -0,0 +1,20 @@
+﻿// 125. Valid Palindrome
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+
+bool isPalindrome(char * s){
+    int len = strlen(s);
+    if(!len || len ==1)
+        return true;
+    char* t = s + (len-1);
+    while(s<t){
+        if(!isalnum(*s)){s++;continue;}
+        if(!isalnum(*t)){t--;continue;}
+        if(tolower(*s++) != tolower(*t--)) return false;
+    }
+    return true;
+}
